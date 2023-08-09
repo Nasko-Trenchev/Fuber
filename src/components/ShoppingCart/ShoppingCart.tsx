a46dc078091348177cd42db08522f09085fa987b
Nasko Trenchev <naskotrenchev1@gmail.com>
2023-08-09 13:33:43 +0300

    Removing GoogleAPIKey

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,11 +1,12 @@
-import { StyledEngineProvider, Button } from '@mui/material';
+import { StyledEngineProvider, Button, IconButton } from '@mui/material';
 import { Order } from '../../contexts/OrderContext';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 
 import styles from './ShoppingCart.module.css';
 
 export default function ShoppingCart() {
 
-    const { order, removeFromOrder } = Order();
+    const { order, removeFromOrder, setShowOrder } = Order();
 
     const total = order.reduce((acc, curr) =>
         acc + (curr.prize * curr.count), 0
@@ -15,12 +16,20 @@ export default function ShoppingCart() {
         <StyledEngineProvider injectFirst>
             {order.length > 0 &&
                 <div className={styles["shoppingCart"]}>
+                    <IconButton
+                        size='small'
+                        onClick={() => setShowOrder(false)}
+                        sx={{ ml: 'auto' }}
+                    >
+                        <CancelOutlinedIcon 
+                        sx={{color: 'black'}} />
+                    </IconButton>
                     {order.map(x => (
                         <div key={x.imageUrl} className={styles["shoppingCartDetails"]}>
                             <img src={x.imageUrl} alt="" />
                             <p>{x.count}x {x.title}</p>
                             <p>{x.count * x.prize} лв</p>
-                            <Button onClick={() => removeFromOrder(x.title)} sx={{ color: 'red', ":hover": {backgroundColor: 'whitesmoke'} }}>X</Button>
+                            <Button onClick={() => removeFromOrder(x.title)} sx={{ color: 'red', ":hover": { backgroundColor: 'whitesmoke' } }}>X</Button>
                         </div>
                     ))}
                     <div className={styles["shoppingCartChekout"]}>
